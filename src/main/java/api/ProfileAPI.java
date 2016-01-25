8f5f78537c2fb002ae748018b5005923188d9a2a
hgrourou <hgrourou>
2016-01-25 11:59:18 +0800

    a simple framework.

---
+++ src/main/java/api/ProfileAPI.java
@@ -0,0 +1,8 @@
+package api;
+
+/**
+ * Created by wang kang on 2016/1/23.
+ */
+public class ProfileAPI {
+
+}
---
+++ src/main/java/init/SecurityConfig.java
@@ -0,0 +1,92 @@
+package init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders
+        .AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration
+        .EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration
+        .WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import security.AuthenticationTokenProcessingFilter;
+import com.mode.security.CsrfSecurityRequestMatcher;
+import com.mode.security.UnauthorizedEntryPoint;
+import com.mode.security.UserDetailsServiceImpl;
+
+/**
+ * This is the java annotations based configurations for Spring Security.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean(name = "authenticationManager")
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean(name = "userDetailsService")
+    protected UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean(name = "unauthorizedEntryPoint")
+    protected UnauthorizedEntryPoint unauthorizedEntryPoint() {
+        return new UnauthorizedEntryPoint();
+    }
+
+    @Bean(name = "csrfSecurityRequestMatcher")
+    protected CsrfSecurityRequestMatcher csrfSecurityRequestMatcher() {
+        return new CsrfSecurityRequestMatcher();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new AuthenticationTokenProcessingFilter(userDetailsService()),
+                        UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeRequests().antMatchers("/v2/signup", "/v2/login","/v2/stylistDrafts/imageURL","/v2/items/**",
+                "/v2/stylistDrafts/taggifiedImages/**").permitAll()
+                .anyRequest().hasAnyAuthority("MERCHANT","ADMIN");
+
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(unauthorizedEntryPoint())
+                .accessDeniedPage("/error/403");
+
+        csrfSecurityRequestMatcher().setExecludeUrls(excludedUrls());
+        http.csrf().requireCsrfProtectionMatcher(csrfSecurityRequestMatcher());
+    }
+
+    /*
+     * Add the excluded urls for csrf
+     */
+    private List<String> excludedUrls() {
+        List<String> execludeUrls = new ArrayList<String>();
+
+        execludeUrls.add("/v2");
+
+        return execludeUrls;
+    }
+}
