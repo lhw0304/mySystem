05559be212151c33ca0b1bc6f49a53d99279d4ba
hgrourou <hgrourou>
2016-01-29 18:22:52 +0800

    remove useless folder.

--- config.groovy
+++ config.groovy
@@ -7,23 +7,14 @@ environments {
             maxActive = '5'
             initialSize = '1'
         }
-
         log4j {
             level = 'DEBUG'
         }
-
         thread {
             maxPoolSize = 10
             corePoolSize = 3
             queueCapacity = 100
             keepAliveSeconds = 300
-        }
-
-        uri {
-            imageService = 'http://localhost:8080/platform-2.0-image'
-            recommenderService = 'http://localhost:8080/platform-2.0-recommender'
-            logService = 'http://localhost:8080/platform-2.0-log'
-            webService = 'http://localhost:8080/web'
         }
     }
 
