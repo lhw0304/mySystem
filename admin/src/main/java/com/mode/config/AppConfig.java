07b3725cefcf166d2cc7b66a4c06e876acc6ef18
hgrourou <hgrourou>
2016-02-01 19:24:18 +0800

    add admin.

---
+++ admin/src/main/java/com/mode/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.mode.config;
+
+/**
+ * Global configurations for the application.
+ *
+ * @author chao
+ */
+public class AppConfig extends BaseConfig {
+
+    // Define headline parameters
+    public final static Integer HEADLINE_DEFAULT_SORT_ORDER = 100;
+
+}
